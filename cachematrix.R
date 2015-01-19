749ca0a836e4fe36893d07c016b07836d5a0826b
Paul Leach <pjl-dev@outlook.com>
2015-01-19 00:34:41 -0800

    My first version of programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makeCacheMatrix  -- constructor for matrix object with cacheable inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  # m is the current value of the inverse if one has been set; initially NULL
+  m <- NULL
+  # set the value of the matrix
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  # get the value of the matrix
+  get <- function() x
+  # set the inverse of the matrix
+  setinv <- function(inv) m <<- inv
+  # get the inverse of the matrix
+  getinv <- function() m
+  
+  # return the list of funtions to manipulate the matrix
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve -- Return a matrix that is the inverse of 'x'
+##  using a cached copy if available
+cacheSolve <- function(x, ...) { 
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinv(m)
+  m
 }
+ 
